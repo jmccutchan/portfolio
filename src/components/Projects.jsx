134d96a9343a9be299a0e03cdbfba81c59cfe661
jmccutchan <mccutchanjames@gmail.com>
2025-04-22 11:07:00 -0400

    added projects style button

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
             Projects
         </h2>
         <div className='grid md:grid-cols-2 gap-4 mt-4'>
-            <div className='p-4 bg-blue-900 rounded'><Link href="https://next-jobs-nu.vercel.app/">Next Jobs</Link></div>
+            <div className='p-4 bg-blue-900 rounded w-full text-white hover:bg-amber-300 hover:text-blue-700'><Link href="https://next-jobs-nu.vercel.app/">Next Jobs</Link></div>
             {/* <div className='p-4 bg-blue-900 rounded'>Project 2</div>
             <div className='p-4 bg-blue-900 rounded'>Project 3</div>
             <div className='p-4 bg-blue-900 rounded'>Project 4</div> */}
