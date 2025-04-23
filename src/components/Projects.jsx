a3ec683be5dd08af512894942cb212a67d669ab7
jmccutchan <mccutchanjames@gmail.com>
2025-04-23 13:17:21 -0400

    added devhub

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,6 +9,7 @@ const Projects = () => {
         </h2>
         <div className='grid md:grid-cols-2 gap-4 mt-4'>
             <div className='p-4 bg-blue-900 rounded w-full text-white hover:bg-amber-300 hover:text-blue-700'><Link href="https://next-jobs-nu.vercel.app/">Next Jobs</Link></div>
+            <div className='p-4 bg-blue-900 rounded w-full text-white hover:bg-amber-300 hover:text-blue-700'><Link href="https://devhub-liart.vercel.app/">Devhub</Link></div>
             {/* <div className='p-4 bg-blue-900 rounded'>Project 2</div>
             <div className='p-4 bg-blue-900 rounded'>Project 3</div>
             <div className='p-4 bg-blue-900 rounded'>Project 4</div> */}
